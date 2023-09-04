3b7bbf40a727103505eaa163529e1e1004f95375
Danilo Cordeiro <danilodevapp@gmail.com>
2023-09-04 10:22:44 -0300

    Primeiro Desafio Ruby DIO

---
+++ desafio01.rb
@@ -0,0 +1,15 @@
+puts ""
+puts "Sistema da Biblioteca Soberana!"
+puts ""
+print "Digite seu Nome: "
+nome = gets.chomp
+puts ""
+print "Digite seu Sobrenome: "
+sobrenome = gets.chomp
+puts ""
+print "Digite sua Idade: "
+idade = gets.chomp
+puts ""
+puts "Novo usuário: " + nome + " " + sobrenome + ", " + idade + " anos"
+puts "Mais um leitor, a Patria Amada Agradece! (~(:)§(:)~)"
+puts "                                             <->"
---
+++ entrada.rb
@@ -0,0 +1,4 @@
+puts "Olá!"
+puts "Digite o seu nome:"
+nome = gets.chomp
+puts "O seu nome é: " + nome
